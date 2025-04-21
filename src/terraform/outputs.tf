42337856f27c333de9eab13e146281bdbd8ab680
Mark Tinderholt <markti@outlook.com>
2025-04-21 10:42:13 -0400

    Managed by Terraform

---
+++ src/terraform/outputs.tf
@@ -0,0 +1,6 @@
+output "primary_function_name" {
+  value =  module.region_stamp_primary.function_name
+}
+output "primary_resource_group_name" {
+  value = module.region_stamp_primary.resource_group_name
+}
