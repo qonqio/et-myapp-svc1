2741f53e106fbe29e858554f86f7138c7d87ea5a
Mark Tinderholt <markti@outlook.com>
2025-04-21 10:41:15 -0400

    Managed by Terraform

---
+++ src/dotnet/Contoso.MyApp.FunctionHost/PingModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contoso.MyApp.FunctionHost
+{
+    public class PingModel
+    {
+        public string Answer { get; set; }
+        public string Version { get; set; }
+    }
+}
