52b597d854eff1c894c4ff615dc5d534f04eedc8
markti <markti@outlook.com>
2025-04-23 11:20:18 -0400

    change name

--- src/terraform/regions.tf
+++ src/terraform/regions.tf
@@ -4,10 +4,10 @@ module "region_stamp_primary" {
   source = "./modules/regional-stamp"
 
   location                        = var.primary_location
-  application_name                = "${var.application_name}${var.environment_suffix}"
+  application_name                = var.application_name
   environment_name                = var.environment_name
   core_name                       = var.core_name
-  name                            = "${var.application_name}-${var.environment_name}${var.environment_suffix}"
+  name                            = "${var.application_name}${var.environment_suffix}-${var.environment_name}"
   number                          = 1
   os_type                         = var.os_type
   tags                            = local.all_tags
